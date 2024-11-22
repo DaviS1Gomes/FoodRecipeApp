95c7235099d957287520b895aace28ad46218c79
Davi Gomes <daviszgomes@gmail.com>
2024-11-22 17:16:05 -0300

    Search List from RecipeList finished

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,1 +1,42 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
+
+
+
+const SearchFilter = ({ icon, placeholder }) => {
+    return(
+        <View style={styles.container}>
+            <FontAwesome name={icon} size={20} color={"#EEAD2D"} />
+            <TextInput style={styles.textMain}>{placeholder} </TextInput>
+        </View>
+    )
+}
+
+export default SearchFilter;
+
+const styles=StyleSheet.create({
+
+container:{
+    backgroundColor:"#fff",
+    flexDirection:'row',
+    paddingVertical:16,
+    borderRadius:8,
+    paddingHorizontal:16,
+    marginVertical: 16,
+    alignItems:'center',
+    gap:5,
+
+    shadowColor:"#000",
+    shadowOffset:{width: 0, height:4},
+    shadowOpacity:0.1,
+    shadowRadius:7,
+},
+textMain:{
+    paddingLeft: 8, 
+    fontSize: 16,
+    color:"#808080"
+}
+
+
+})
--- src/screens/RecipeListScreen.js
+++ src/screens/RecipeListScreen.js
@@ -1,11 +1,13 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native"
 import React from 'react'
 import Header from '../components/Header'
+import SearchFilter from "../components/SearchFilter" 
 
 const RecipeListScreen = () =>{
   return(
     <SafeAreaView style={styles.recipeSafeAreaView}>
-      <Header headerText={"Hello"} headerIcon={"bell-o"}/>
+      <Header headerText={"Lista de Receita"} headerIcon={"bell"}/>
+    <SearchFilter icon ="search" placeholder={"bote sua receita favorita"}/>
     </SafeAreaView>
   )
 }
